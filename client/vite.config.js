ec5b44c1a5483002dd5edcd758bcc675eccddc56
GabDiocadiz <gddiocadiz@up.edu.ph>
2025-05-01 07:41:25 +0800

    chore: added backend url to target

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,4 +7,13 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), flowbiteReact()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://artemis-7cxd.onrender.com' || 'http://localhost:5050',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
