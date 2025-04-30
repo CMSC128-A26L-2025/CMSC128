9a173ad3339aa6a47aa69c3d61ca456461479f00
GabDiocadiz <gddiocadiz@up.edu.ph>
2025-04-30 10:37:49 +0800

    feat: added resolve alias and server proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,4 +7,19 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://artemis-7cxd.onrender.com', // Change this to your backend server URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
