3c608427e905867cc8db876e63e302e5ec4ba418
GabDiocadiz <gddiocadiz@up.edu.ph>
2025-05-05 20:50:32 +0800

    test: _redirects

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,19 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), flowbiteReact()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    flowbiteReact(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.'
+        }
+      ]
+    })
+  ],
   server: {
     proxy: {
       '/api': {
