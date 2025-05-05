c9ebd8d9e0e163911fb9d5f3b3fa56efd8c27058
GabDiocadiz <gddiocadiz@up.edu.ph>
2025-05-05 20:56:36 +0800

    feat: import viteStaticCopy haha

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 
 // https://vite.dev/config/
